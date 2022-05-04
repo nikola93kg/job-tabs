2e0e8b949871df1f89d1ca2e719bdab2283b4971
Nikola Markovic <53098565+nikola93kg@users.noreply.github.com>
2022-05-04 22:08:42 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineDoubleRight } from "react-icons/ai";
+import { uid } from './unique-id';
+const url = 'https://course-api.com/react-tabs-project';
+
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, [])
+
+
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+
+  const { company, dates, duties, title } = jobs[value];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {
+            jobs.map((job, index) => {
+              return <button key={job.id} onClick={() => setValue(index)} className={`job-btn ${index === value && 'active-btn'}`}>
+                {job.company}
+              </button>
+            })
+          }
+        </div>
+
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {
+            duties.map((duty, index) => {
+              return <div key={index} className="job-desc">
+                <AiOutlineDoubleRight className='job-icon' />
+                <p>{duty}</p>
+              </div>
+            })
+          }
+        </article>
+      </div>
+    </section>
+  )
 }
 
-export default App;
+export default App
